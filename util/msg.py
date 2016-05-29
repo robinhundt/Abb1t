891177e2d0e37f432fb0e534bb8f29d279f16151
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2016-05-29 20:04:54 +0200

    more parsing options

--- util/msg.py
+++ util/msg.py
@@ -3,12 +3,22 @@ class Msg:
     def __init__(self, raw_msg):
         self.raw_msg=raw_msg
 
-    def get_recv_id(self):
+
+    """get the id of the person that sent the message"""
+    def get_sent_id(self):
         try:
             return self.raw_msg['from']['id']
         except KeyError:
             return ''
 
+    """get the id of the group/person that the message was sent in"""
+    def get_chat_id(self):
+        try:
+            return self.raw_msg['chat']['id']
+        except KeyError:
+            return ''
+
+    """get the text of the message"""
     def get_text(self):
         try:
             return self.raw_msg['text']
