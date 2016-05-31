c65ffc4f90da25fd64719ce631cfd82dc1cdc214
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2016-05-31 20:04:54 +0200

    Add 'template.py' as a basis for new modules
    Copy and paste it in order to create a new module. Keep in mind to change the class name as well as the self.name string. Add it to your config.ini in order to load it.

---
+++ mods/template.py
@@ -0,0 +1,23 @@
+#essential
+import _thread as thread
+from queue import *
+
+class template:
+    def __init__(self, bot):
+        self.name="template"
+        self.bot = bot
+        self.queue_in=Queue()
+        #self.queue_out=Queue()
+        thread.start_new_thread(self.run,())
+        #self.resttime=0
+        #self.lastcmd=0
+
+    def run(self):
+        while 1: 
+            msg=self.queue_in.get() # get() is blocking
+            chat_id=msg.get_chat_id()
+            if msg.get_text()=="/test":
+                self.bot.sendMessage(chat_id,"Hello World!")
+
+    def enqueue(self,msg):
+        self.queue_in.put(msg)
