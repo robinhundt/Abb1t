bfadcec286f1876137dfebb93cd31402c484a30a
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2016-05-31 20:13:28 +0200

    Add 'translate' module that translate given input to german

---
+++ mods/translate.py
@@ -0,0 +1,31 @@
+#essential
+import _thread as thread
+from queue import *
+
+import textblob
+
+class translate:
+    def __init__(self, bot):
+        self.name="template"
+        self.bot = bot
+        self.queue_in=Queue()
+        #self.queue_out=Queue()
+        thread.start_new_thread(self.run,())
+        #self.resttime=0
+        #self.lastcmd=0
+
+    def run(self):
+        while 1: 
+            msg=self.queue_in.get() # get() is blocking
+            chat_id=msg.get_chat_id()
+            if msg.get_text()[:len("/translate")]=="/translate" and len(msg.get_text().split(" "))>=2:
+                foreign=msg.get_text().split(" ",1)[1]
+                try:
+                    b=textblob.TextBlob(foreign)
+                    reply="{}".format(b.translate(to="de"))
+                except textblob.exceptions.NotTranslated:
+                    reply="Error while translation. (Are there smileys in some words?)"
+                self.bot.sendMessage(chat_id,reply)
+
+    def enqueue(self,msg):
+        self.queue_in.put(msg)
