bd71df868812da5db5529bd4274eec9f1a56568e
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2016-06-01 14:14:10 +0200

    Add 'help' module, which generates usage information
    Each module has a description, which will be given regarding to the whitelist and blacklist system. If the description is empty, the module is loaded, but not included into the help message.

---
+++ mods/help.py
@@ -0,0 +1,36 @@
+#essential
+import _thread as thread
+from queue import *
+
+class help:
+    def __init__(self, bot):
+        self.bot = bot.bot
+        self.description = "*/help* - outputs this help message"
+        self.config = bot
+        self.queue_in=Queue()
+        #self.queue_out=Queue()
+        thread.start_new_thread(self.run,())
+        #self.resttime=0
+        #self.lastcmd=0
+
+    def run(self):
+        while 1: 
+            msg=self.queue_in.get() # get() is blocking
+            chat_id=msg.get_chat_id()
+            reply=""
+            if msg.get_text()=="/help":
+                for m in self.config.mods:
+                    if chat_id in self.config.whitelist:
+                        if not type(m).__name__ in self.config.whitelist[chat_id]:
+                            try:
+                                if (m.description): # you are able to hide modules with this
+                                    reply+="\n{}".format(m.description)
+                            except AttributeError:
+                                reply+="\nClass *{}* has no description yet".format(type(m).__name__)
+                        else:
+                            print("not allowed")
+            if reply:
+                self.bot.sendMessage(chat_id,"*Usable modules for this group:*{}".format(reply),parse_mode="Markdown")
+
+    def enqueue(self,msg):
+        self.queue_in.put(msg)
