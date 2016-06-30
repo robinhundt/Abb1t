70e58302f2749bde87ef52f10f974ee5c5e56d93
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2016-06-30 13:30:25 +0200

    Change xkcd.py: delete camelcase, add live ticker
    Now it is possible to subscribe to xkcd module in order to get the newest xkcd.

--- mods/xkcd.py
+++ mods/xkcd.py
@@ -8,6 +8,7 @@ import re
 import requests
 import random
 import tempfile
+import time
 
 xkcd_latest = "http://xkcd.com/info.0.json"
 xkcd_number = "http://xkcd.com/{}/info.0.json"
@@ -16,33 +17,63 @@ class xkcd:
 
     def __init__(self, bot):
         self.bot = bot.bot
-        self.description = r"""^(\/xkcd) ?([1-9]\d+|rnd|rand|random|)$"""
+        self.description = r"""/xkcd ?([1-9]\d+|rnd|rand|random|toggle|)$ - self explanatory, possibility to toggle for new xkcds"""
         self.queue_in = Queue()
+        self.latest = xkcd.get_total_number()
+        self.chat_ids = []
+        try: 
+            with open("xkcd_chat_ids") as f:
+                chat_ids=f.read()
+                if chat_ids:
+                    self.chat_ids=[int(chat_id) for chat_id in chat_ids.split(",")]
+        except FileNotFoundError:
+            pass
         thread.start_new_thread(self.run, ())
+        thread.start_new_thread(self.update, ())
 
     def run(self):
         while True:
             message = self.queue_in.get()  # get() is blocking
-            message_text = message.get_text()
+            message_text = message.get_text().lower()
             chat_id = message.get_chat_id()
             match = re.search(r'^(\/xkcd) ?([1-9]\d+|rnd|rand|random|)$', message_text)
-            if not match:
-                continue
+            if match:
+                if not match.group(2):
+                    self.getXKCD(chat_id, xkcd.get_total_number())
+                elif 'nd' in match.group(2):
+                    random.seed()
+                    self.getXKCD(chat_id, random.randint(1, xkcd.get_total_number()))
+                elif int(match.group(2)) < xkcd.get_total_number():
+                    self.getXKCD(chat_id, int(match.group(2)))
+                else:
+                    self.bot.sendMessage(chat_id, "Sorry, this xkcd doesn't yet exist.")
+            elif re.search(r'^/xkcd toggle$', message_text):
+                print(self.chat_ids)
+                if chat_id not in self.chat_ids:
+                    self.chat_ids.append(message.get_chat_id())
+                    self.bot.sendMessage(message.get_chat_id(), "Added your chat_id to the *xkcd ticker*", parse_mode="Markdown")
+                else:
+                    self.chat_ids.remove(message.get_chat_id())
+                    self.bot.sendMessage(message.get_chat_id(), "Deleted your chat_id from the *xkcd ticker*", parse_mode="Markdown")
+                self.save_ids()
 
-            if not match.group(2):
-                self.getXKCD(chat_id, getTotalNumber())
-            elif 'nd' in match.group(2):
-                random.seed()
-                self.getXKCD(chat_id, random.randint(1, getTotalNumber()))
-            elif int(match.group(2)) < getTotalNumber():
-                self.getXKCD(chat_id, int(match.group(2)))
-            else:
-                self.bot.sendMessage(chat_id, "Sorry, this xkcd doesn't yet exist.")
-
+    def save_ids(self):
+        with open("xkcd_chat_ids","w") as fw:
+            fw.write(",".join(str(chat_id) for chat_id in self.chat_ids))
 
 
     def enqueue(self, msg):
         self.queue_in.put(msg)
+
+
+    def update(self):
+        while True:
+            if self.chat_ids: # refresh only if someone needs the xkcd...
+                current_latest = xkcd.get_total_number()
+                if self.latest != current_latest: #new one available
+                    self.latest = current_latest
+                    self.getXKCD(chat_id, xkcd.get_total_number())
+            time.sleep(900) # sleep 15min
 
     def getXKCD(self, chat_id, number):
         r = requests.get(xkcd_number.format(number)).json()
@@ -54,8 +85,8 @@ class xkcd:
             self.bot.sendPhoto(chat_id, image)
             self.bot.sendMessage(chat_id, r[u'alt'])
 
-
-def getTotalNumber():
-    return int(requests.get(xkcd_latest).json()['num'])
+    @staticmethod
+    def get_total_number():
+        return int(requests.get(xkcd_latest).json()['num'])
 
 
