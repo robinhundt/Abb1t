062159f6a8c8908ccd6b24bd661041b076936de9
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2016-08-19 10:56:36 +0200

    Shorten some dict queries

--- util/msg.py
+++ util/msg.py
@@ -1,8 +1,6 @@
 class Msg:
-    """docstring for msg"""
     def __init__(self, raw_msg):
         self.raw_msg=raw_msg
-
 
     """get the id of the person that sent the message"""
     def get_sent_id(self):
@@ -37,10 +35,7 @@ class Msg:
 
     """get the message_id of the chat message, to reply to it"""
     def get_message_id(self):
-        try: 
-            return self.raw_msg['message_id']
-        except KeyError:
-            return ''
+        return self.raw_msg.get('message_id','')
 
     """
     get the type of the chat that the message was sent in
@@ -52,24 +47,14 @@ class Msg:
         except KeyError:
             return ''
 
-
     """get the timestamp of the chat message"""
     def get_date(self):
-        try:
-            return self.raw_msg['date']
-        except KeyError:
-            return 0
+        return self.raw_msg.get('date',0)
 
     """get the edit timestamp of the message"""
     def get_edit_date(self):
-        try:
-            return self.raw_msg['edit_date']
-        except KeyError:
-            return 0
+        return self.raw_msg.get('edit_date',0)
 
     """get the text of the message"""
     def get_text(self):
-        try:
-            return self.raw_msg['text']
-        except KeyError:
-            return ''
+        return self.raw_msg.get('text','')
