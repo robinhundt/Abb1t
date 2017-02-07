d0b1a530506255a6d9eaa3ac62a338699d2de83c
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2017-02-07 21:35:51 +0100

    Add changed class of headlines of freebies

--- mods/mydealz.py
+++ mods/mydealz.py
@@ -94,7 +94,7 @@ class mydealz:
             if self.chat_ids: # refresh only if someone needs the freebie...
                 freebies = []
                 temperatures = self.get_xpath(mydealz.url,"//strong[contains(@class,'vote-temp tGrid-cell vAlign--all-m text--b')]")
-                for a,temp in zip(self.get_xpath(mydealz.url,'//*[@class="cept-tt linkPlain space--r-1 space--v-1"]'),temperatures):
+                for a,temp in zip(self.get_xpath(mydealz.url,'//*[@class="vwo-thread-title cept-tt linkPlain space--r-1 space--v-1"]'),temperatures):
                     if int(temp.text[:-1])>=mydealz.min_temp:
                         freebies.append(a.text)
                         if self.freebies and a.text not in self.freebies: #set, and new freebie
