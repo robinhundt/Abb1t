c75d6df149a149ff6919eb3b2d0b5c22c58e5396
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2017-04-26 19:54:01 +0200

    Add sh file to install phantomjs for RaspPi

---
+++ install_phantomjs.sh
@@ -0,0 +1,6 @@
+wget https://github.com/aeberhardo/phantomjs-linux-armv6l/archive/master.zip
+unzip master.zip
+cd phantomjs-linux-armv6l-master
+bunzip2 *.bz2 && tar xf *.tar
+sudo cp phantomjs-1.9.0-linux-armv6l/bin/phantomjs  /usr/bin
+
